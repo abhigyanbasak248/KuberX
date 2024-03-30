44be6d15cc97320433b692370e8ea812efa7aef1
Dev <dev.thakkar@learner.manipal.edu>
2024-03-30 18:23:29 +0530

    client with server

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
