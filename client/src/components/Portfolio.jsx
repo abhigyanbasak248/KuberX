b35bd201e87afff931d72d4df3b80903d3649126
Sagnik <santrasagnik10@gmail.com>
2024-04-05 03:53:31 +0530

    minor changes

--- client/src/components/Portfolio.jsx
+++ client/src/components/Portfolio.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import TradingViewWidget from "../components/TradingViewWidget";
 
-function Stock() {
-  const indices = ["NASDAQ:AAPL", "NASDAQ:GOOGL", "NASDAQ:AMZN", "NASDAQ:MSFT"];
+function Portfolio() {
+  const indices = ["NASDAQ:NVDA", "NASDAQ:MSFT"];
   return (
     <div className="container mx-auto p-4 grid grid-cols-1 gap-4 md:grid-cols-2">
       {indices.map((index) => (
-        <div key={index} className="p-4 border-2 border-green-400 rounded-lg min-h-[24rem]">
+        <div key={index} className="p-4 border-2 rounded-lg min-h-[24rem] w-full">
           <TradingViewWidget symbol={index} />
         </div>
       ))}
@@ -14,4 +14,4 @@ function Stock() {
   );
 }
 
-export default Stock;
+export default Portfolio;
