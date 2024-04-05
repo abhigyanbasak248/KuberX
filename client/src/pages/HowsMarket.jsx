4a956d294a6d2952a5708d748ee8582262684699
shrudex <codeshubh@gmail.com>
2024-04-06 00:41:50 +0530

    hows market page

--- client/src/pages/HowsMarket.jsx
+++ client/src/pages/HowsMarket.jsx
@@ -4,21 +4,28 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 import { useState } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { PropagateLoader } from "react-spinners";
 
 const style = {
   position: "absolute",
-  top: "50%",
+  top: "45%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: 400,
-  bgcolor: "background.paper",
-  border: "2px solid #000",
+  width: "50%",
+  bgcolor: "#233601",
+  border: "3px solid white",
+  borderRadius: "15px",
   boxShadow: 24,
   p: 4,
 };
 
 const HowsMarket = () => {
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState("");
+  const [data, setData] = useState([]);
+  const [response, setResponse] = useState("");
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
@@ -56,13 +63,65 @@ const HowsMarket = () => {
               </svg>
             </button>
             <Modal
+              className="w-full"
               open={open}
               onClose={handleClose}
               aria-labelledby="modal-modal-title"
               aria-describedby="modal-modal-description"
             >
               <Box sx={style}>
-                <form className="max-w-md mx-auto">
+                <form
+                  className="max-w-md mx-auto"
+                  onSubmit={async (e) => {
+                    e.preventDefault();
+                    setResponse(null);
+                    try {
+                      const response = await axios.get(
+                        `https://newsapi.org/v2/everything?q=${value}&sortBy=publishedAt&apiKey=837125b0ac2a44b788ff61164a8ab046`
+                      );
+
+                      toast.loading(`Fetching market summary for ${value}...`);
+                      let combinedText = "";
+
+                      for (let i = 0; i < response.data.articles.length; i++) {
+                        const { content, description, title } =
+                          response.data.articles[i];
+                        const combinedLine = `${content} ${description} ${title}`;
+                        const cleanedLine = combinedLine.replace(
+                          /[^\w\s]/g,
+                          ""
+                        );
+                        const cleanedLength = combinedText.replace(
+                          /\s/g,
+                          ""
+                        ).length;
+                        if (cleanedLength + cleanedLine.length > 10000) {
+                          break;
+                        } else {
+                          combinedText += cleanedLine + " ";
+                        }
+                      }
+
+                      console.log(combinedText.trim());
+                      try {
+                        const sentimentResponse = await axios.post(
+                          `http://127.0.0.1:5000//market/${value}`,
+                          {
+                            text: combinedText.trim(),
+                          }
+                        );
+                        console.log(sentimentResponse);
+                        toast.dismiss();
+                        setResponse(sentimentResponse.data.summary);
+                        toast.success("Market summary fetched successfully!");
+                      } catch (err) {
+                        console.log(err);
+                      }
+                    } catch (err) {
+                      console.log(err);
+                    }
+                  }}
+                >
                   <label
                     htmlFor="default-search"
                     className="mb-2 text-sm font-medium text-gray-900 sr-only"
@@ -90,18 +149,29 @@ const HowsMarket = () => {
                     <input
                       type="search"
                       id="default-search"
-                      className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 placeholder-gray-400"
-                      placeholder="Search Mockups, Logos..."
+                      className="block w-full p-4 ps-10 text-md text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:outline-none placeholder-gray-400"
+                      placeholder="Search Stocks, Companies..."
                       required
+                      value={value}
+                      onChange={(e) => setValue(e.target.value)}
                     />
                     <button
                       type="submit"
-                      className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2"
+                      className="text-white absolute end-2.5 bottom-2.5 bg-green-700 hover:bg-green-800 focus:outline-none font-medium rounded-lg text-sm px-4 py-2"
                     >
                       Search
                     </button>
                   </div>
                 </form>
+                <div className="h-auto w-full">
+                  {response === null ? (
+                    <div className="flex h-20 justify-center items-center mt-4">
+                      <PropagateLoader color="#ffffff" />
+                    </div>
+                  ) : (
+                    <div className="">{response}</div>
+                  )}
+                </div>
               </Box>
             </Modal>
           </div>
