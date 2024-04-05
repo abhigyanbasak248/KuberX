127ec4094fdd015ff7d30b325378103299ba4248
abhigyanbasak248 <abhigyanbasak248@gmail.com>
2024-04-05 14:24:17 +0530

    added model

--- client/app.py
+++ client/app.py
@@ -17,6 +17,13 @@ from langchain.chains import RetrievalQA, ConversationalRetrievalChain, LLMChain
 from constants import CHROMA_SETTINGS 
 from dotenv import load_dotenv
 
+from keras.applications import efficientnet
+from keras.applications.efficientnet import preprocess_input
+from tensorflow.keras.models import load_model
+import numpy as np
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import load_model
 
 app = Flask(__name__)
 CORS(app)
@@ -29,7 +36,31 @@ llm = ChatOpenAI(openai_api_key=os.environ["OPENAI_API_KEY"], temperature=0.1, m
 
 translator = Translator()
 
-@app.route('/chatbot/<string:instruction>/<string:source>/<string:des>', methods = ['POST'])
+classes = ['All Beauty',
+ 'All Electronics',
+ 'Appliances',
+ 'Arts, Crafts & Sewing',
+ 'Automotive',
+ 'Baby',
+ 'Baby Products',
+ 'Beauty',
+ 'Cell Phones & Accessories',
+ 'Clothing, Shoes & Jewelry',
+ 'Electronics',
+ 'Grocery & Gourmet Food',
+ 'Health & Personal Care',
+ 'Industrial & Scientific',
+ 'Musical Instruments',
+ 'Office Products',
+ 'Patio, Lawn & Garden',
+ 'Pet Supplies',
+ 'Sports & Outdoors',
+ 'Tools & Home Improvement',
+ 'Toys & Games']
+
+model = load_model('client/model.h5')
+
+@app.route('/chatbot/<string:instruction>/<string:source>/<string:des>', methods = ['GET'])
 def chatgpt_call(instruction, source, des, ch):
 #     memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 #     embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
@@ -89,5 +120,36 @@ def chatgpt_call(instruction, source, des, ch):
     print(ans)
     return ans
 
+def read_image(filename):
+    img = load_img(filename, target_size=(224, 224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    return x
+
+@app.route('/predict/<string:file_path>',methods=['GET'])
+def classify(file_path):
+    # if request.method == 'POST':
+    #     file = request.files['file']
+    #     if file and allowed_file(file.filename): #Checking file format
+    #         filename = file.filename
+    #         file_path = os.path.join('static/images', filename)
+    #         file.save(file_path)
+    file_path = file_path.replace('-', '/')
+    file_path = '/'+file_path
+    print(file_path)
+    img = read_image(file_path) #prepressing method
+    class_prediction=model.predict(img) 
+    classes_x=np.argmax(class_prediction[0])
+    confidence = str(class_prediction[0][classes_x])
+    pred = classes[classes_x]
+    ans = {
+        "result" : pred,
+        "confidence" : confidence
+    }
+    print(ans)
+    return ans
+
+# app.route('*')
 if __name__ == "__main__":
    app.run(debug = True)
